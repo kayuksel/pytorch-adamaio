3b11ff905ca3c58fa75dbfd87ae158cca858fbb4
kayuksel <46922047+kayuksel@users.noreply.github.com>
2019-01-22 18:32:24 +0100

    Add files via upload

---
+++ AdamAIO.py
@@ -0,0 +1,40 @@
+import math
+import torch
+from torch.optim.optimizer import Optimizer
+
+class AdamAIO(Optimizer):
+    def __init__(self, params, lr=2e-3, betas=(0.9, 0.999), eps=5e-3, weight_decay=5e-6, hypergrad=1e-7, partial=0.75):#1e-7
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, hypergrad=hypergrad, partial=partial)
+        super().__init__(params, defaults)
+    def step(self, closure=None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None: continue
+                grad = p.grad.data
+                state = self.state[p]
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+                state['step'] += 1
+                if group['hypergrad'] > 0 and state['step'] > 1:
+                    prev_bias_correction1 = 1 - beta1 ** (state['step'] - 1)
+                    prev_bias_correction2 = 1 - beta2 ** (state['step'] - 1)
+                    h = torch.dot(grad.view(-1), torch.div(exp_avg, exp_avg_sq.sqrt().add_(group['eps'])).view(-1)) * math.sqrt(prev_bias_correction2) / prev_bias_correction1
+                    group['lr'] += group['hypergrad'] * h
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                denom = exp_avg_sq.sqrt().add_(group['eps'])
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
+                step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
+                if group['weight_decay'] != 0:
+                    decayed_weights = torch.mul(p.data, group['weight_decay'])
+                    p.data.addcdiv_(-step_size, exp_avg, denom**group['partial'])
+                    p.data.sub_(decayed_weights)
+                else:
+                    p.data.addcdiv_(-step_size, exp_avg, denom**group['partial'])
+        return loss
